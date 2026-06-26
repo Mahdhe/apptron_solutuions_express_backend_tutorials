92e64b3b5b32615b92f445ddee69e67f1a94dbfb
Mahdhe <mmmahdhe@gmail.com>
2026-06-26 10:57:48 +0530

    Middleware added: add logger and error handler middleware with global registration

--- src/app.js
+++ src/app.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const app = express();
 
+
+
+const logger = require("./middleware/logger.middleware");
+const errorHandler = require("./middleware/error.middleware");
 const studentRoutes = require("./routes/student.routes");
 const courseRoutes = require("./routes/course.routes");
 const taskRoutes = require("./routes/task.routes");
-const trainerRoutes = require("./routes/trainer.routes")
+const trainerRoutes = require("./routes/trainer.routes");
 
 
+app.use(logger);
 app.use("/api/students", studentRoutes);    
 app.use("/api/courses", courseRoutes);    
 app.use("/api/tasks", taskRoutes);    
 app.use("/api/trainers", trainerRoutes);    
-
+app.use(errorHandler);
 
 module.exports = app;
