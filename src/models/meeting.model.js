caa33d446dcc0a69dc81664716318cbecf13f5b7
Mahdhe <mmmahdhe@gmail.com>
2026-07-16 13:56:31 +0530

    Meeting model created

---
+++ src/models/meeting.model.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const { on } = require("./student.model");
+
+const meetingSchema = new mongoose.Schema (
+    {
+        studentId : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "Student",
+            required : true,
+
+        },
+
+        meetingTitle : {
+            type : String,
+            required : true,
+        },
+
+        meetingDate : {
+            type : String,
+            required : true,
+        },
+
+        meetingTime : {
+            type : String,
+            required : true,
+        },
+
+        meetingType : {
+            type : String,
+            enum : [Online,Offline],
+            required : true,
+        },
+
+        meetingLink : {
+            type : String,
+            required : true,
+        },
+
+        status : {
+            type : String,
+            enum : ["Scheduled","Completed","Cancelled"],
+            default : "Scheduled",
+        },
+
+
+},
+        {
+            timestamps : true,
+        }
+
+
+);
+
+
+module.exports = mongoose.model("Meeting",meetingSchema);
